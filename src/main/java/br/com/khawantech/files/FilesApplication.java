095d0691a642beb04a50a0e324f31699ccabebf7
KhawanC <kauacassiano121@gmail.com>
2026-02-10 21:29:13 -0300

    enable do mongock

--- src/main/java/br/com/khawantech/files/FilesApplication.java
+++ src/main/java/br/com/khawantech/files/FilesApplication.java
@@ -1,11 +1,13 @@
 package br.com.khawantech.files;
 
+import io.mongock.runner.springboot.EnableMongock;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableMongock
 public class FilesApplication {
 
 	public static void main(String[] args) {
